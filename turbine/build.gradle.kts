08870745487ab3015a25bd58ea8bf560544c506b
Andrew Ramires <andrew@tjro.jus.br>
2021-05-03 15:53:27 -0400

    Initial commit

---
+++ turbine/build.gradle.kts
@@ -0,0 +1,48 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.3.10.RELEASE"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	kotlin("jvm") version "1.3.72"
+	kotlin("plugin.spring") version "1.3.72"
+}
+
+group = "br.jus.tjro"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+	mavenCentral()
+}
+
+extra["springCloudVersion"] = "Hoxton.SR11"
+
+dependencies {
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.springframework.cloud:spring-cloud-starter-hystrix-dashboard:1.4.7.RELEASE")
+	implementation("org.springframework.cloud:spring-cloud-starter-netflix-turbine-stream")
+	implementation("org.springframework.cloud:spring-cloud-netflix-hystrix-stream")
+	implementation("org.springframework.cloud:spring-cloud-starter-stream-kafka")
+	implementation("org.springframework.cloud:spring-cloud-starter-sleuth")
+	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+	}
+}
+
+dependencyManagement {
+	imports {
+		mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+	}
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "11"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ turbine/src/main/kotlin/br/andrew/ramires/turbine/TurbineApplication.kt
@@ -0,0 +1,16 @@
+package br.andrew.ramires.turbine
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
+//import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream
+
+@SpringBootApplication
+@EnableHystrixDashboard
+@EnableTurbineStream
+class TurbineApplication
+
+fun main(args: Array<String>) {
+	runApplication<TurbineApplication>(*args)
+}
