a8874122fc3084f29e5c5e54a4b1aa5ad75d44c4
Andrew Ramires <andrew@tjro.jus.br>
2021-05-04 13:27:09 -0400

    registry

--- src/main/kotlin/br/andrew/ramires/turbine/TurbineApplication.kt
+++ src/main/kotlin/br/andrew/ramires/turbine/TurbineApplication.kt
@@ -3,7 +3,6 @@ package br.andrew.ramires.turbine
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
-//import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream
 
 @SpringBootApplication
